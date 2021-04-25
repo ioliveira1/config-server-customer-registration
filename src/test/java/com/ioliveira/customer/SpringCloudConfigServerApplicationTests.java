1dc725ccc27258290a61dd3b43e3d73f06828a47
Igor Oliveira <igorfoliveira@gmail.com>
2021-04-24 21:20:13 -0300

    spring cloud config server customer registration

---
+++ src/test/java/com/ioliveira/customer/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioliveira.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
